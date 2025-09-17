1a0d63aed70d0250a82ac455473948369ebde425
Ha <phamvuhoangha2808@gmail.com>
2025-09-17 21:11:46 +0700

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,47 +1,8 @@
 #include "Header.h"
 
-struct Card
-{
-    enum Rank
-    {
-        rank_ace,
-        rank_2,
-        rank_3,
-        rank_4,
-        rank_5,
-        rank_6,
-        rank_7,
-        rank_8,
-        rank_9,
-        rank_10,
-        rank_jack,
-        rank_queen,
-        rank_king,
-
-        max_ranks
-    };
-
-    enum Suit
-    {
-        suit_club,
-        suit_diamond,
-        suit_heart,
-        suit_spade,
-
-        max_suits
-    };
-
-
-    //These two arrays are used in the "Deck()" constructor function, which iterates through both to create all 52 cards
-    static constexpr std::array allRanks{ rank_ace, rank_2, rank_3, rank_4, rank_5, rank_6, rank_7, rank_8, rank_9, rank_10, rank_jack, rank_queen, rank_king };
-    static constexpr std::array allSuits{ suit_club, suit_diamond, suit_heart, suit_spade };
-
-    //All "Card"-type objects have these 2 variables 
-    Rank rank{};
-    Suit suit{};
-
-    //Overload the operate<< so std::cout would print the full rank & suit of a card when passed a "Card"-type object
-    friend std::ostream& operator<<(std::ostream& out, const Card& card)
+#ifdef FOO
+//Overload the operate<< so std::cout would print the full rank & suit of a card when passed a "Card"-type object
+friend std::ostream& operator<<(std::ostream& out, const Card& card)
     {
         static constexpr std::array ranks{ 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K' };
         static constexpr std::array suits{ 'C', 'D', 'H', 'S' };
@@ -49,10 +10,4 @@ struct Card
         out << ranks[card.rank] << suits[card.suit];
         return out;
     }
-
-    int value() const
-    {
-        static constexpr std::array rankValues{ 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
-        return rankValues[rank];
-    }
-};
+#endif
