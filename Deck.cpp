99e969f064863dd7e7faebb9a8f010b988911d9f
Ha <phamvuhoangha2808@gmail.com>
2025-09-17 21:11:23 +0700

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -1,13 +1,7 @@
 #include "Header.h"
 
-class Deck
-{
-private: //The "Deck"-type object has 2 variables stuck to it: a "Card"-type array of all cards it (can) hold, and an index for the next card to be taken out
-    std::array<Card, 52> m_cards{};
-    std::size_t m_nextCardIndex{ 0 };
 
-public:
-    Deck() //Contructore function to create the m_cards array
+Deck::Deck() //Contructore function to create the m_cards array
     {
         std::size_t count{ 0 };
         for (auto suit : Card::allSuits)
@@ -15,15 +9,14 @@ public:
                 m_cards[count++] = Card{ rank, suit };
     }
 
-    void shuffle() //Shuffle the deck by randomizing the elements of array m_cards
-    {
-        std::shuffle(m_cards.begin(), m_cards.end(), Random::mt);
-        m_nextCardIndex = 0;
-    }
+void Deck::shuffle() //Shuffle the deck by randomizing the elements of array m_cards
+{
+    std::shuffle(m_cards.begin(), m_cards.end(), Random::mt);
+    m_nextCardIndex = 0;
+}
 
-    Card dealCard() //Function returns a "Card"-type object (dealing cards)
-    {
-        assert(m_nextCardIndex != 52); //Make sure that the entire deck has not been dealt already
-        return m_cards[m_nextCardIndex++];
-    }
-};
+Card Deck::dealCard() //Function returns a "Card"-type object (dealing cards)
+{
+    assert(m_nextCardIndex != 52); //Make sure that the entire deck has not been dealt already
+    return m_cards[m_nextCardIndex++]; 
+}
